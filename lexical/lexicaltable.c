4857f706db27ef36bc309f310c99b1d44f6b995c
fhh200000 <1041154549@qq.com>
2022-05-26 22:30:41 +0800

    Update: Gugugu...#3

--- lexical/lexicaltable.c
+++ lexical/lexicaltable.c
@@ -1,1 +1,27 @@
 #include <lexical/lexicaltable.h>
+/*
+ typedef struct {
+    U64 curr;
+    struct {
+        enum {
+            VALUE_RAW,
+            VALUE_CATEGORY
+        } type;
+        U8 value;
+    } input;
+    U64 next;
+} transrule;
+ */
+transrule lexicaltable[] = {
+    {0,{VALUE_RAW,'b'},1}, //byte
+    {0,{VALUE_RAW,'c'},1},
+    {0,{VALUE_RAW,'i'},1},
+    {0,{VALUE_RAW,'l'},1},
+    {0,{VALUE_RAW,'f'},1},
+    {0,{VALUE_RAW,'d'},1},
+    {0,{VALUE_RAW,'i'},1},
+    {0,{VALUE_RAW,'i'},1},
+    {0,{VALUE_RAW,'i'},1},
+    {0,{VALUE_RAW,'i'},1},
+};
+
