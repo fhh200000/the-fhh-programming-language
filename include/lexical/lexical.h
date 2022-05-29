13d90e44b8671a9c583f64b28514c316df1dfaaa
fhh200000 <1041154549@qq.com>
2022-05-29 23:09:14 +0800

    Update:Full lexical table.

--- include/lexical/lexical.h
+++ include/lexical/lexical.h
@@ -2,23 +2,6 @@
 #define LEXICAL_H
 #include <common.h>
 #include <common/tree.h>
-typedef enum {
-    TOKEN_TYPENAME,
-    TOKEN_VARNAME,
-    TOKEN_BRACE_LEFT,
-    TOKEN_BRACE_RIGHT,
-    TOKEN_PARENTHESIS_LEFT,
-    TOKEN_PARENTHESIS_RIGHT,
-    TOKEN_BRACKETS_LEFT,
-    TOKEN_BRACKETS_RIGHT,
-    TOKEN_SEMICOLON,
-    TOKEN_MATH,
-    TOKEN_LOAD,
-    TOKEN_TYPE_CONV,
-    TOKEN_ASSIGN,
-    TOKEN_COMMA,
-    TOKEN_INVALID,
-} token_type;
 typedef struct {
     token_type type;
     U8* begin;
